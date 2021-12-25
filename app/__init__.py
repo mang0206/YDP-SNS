ec6727132ee3e876db992461293c6f7f7a9ec81e
unknown <alswosp0206@naver.com>
2021-12-25 19:33:42 +0900

    [Update] init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ import pymongo
 
 app = Flask(__name__)
 
-conn = pymongo.MongoClient("mongodb://user:ydp111@13.125.216.105:27017/")
+conn = pymongo.MongoClient("mongodb://root:ydp111@13.125.216.105:27017/root")
 
 from app import views
