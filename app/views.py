ffe9952b32915a06e0b77f4463056adb5a4c439d
unknown <alswosp0206@naver.com>
2021-12-27 21:45:56 +0900

    [Update] join, views.py

--- app/views.py
+++ app/views.py
@@ -1,20 +1,23 @@
 from flask import request, render_template, jsonify, redirect, url_for
+import bcrypt
 from . import app, conn
 db = conn.get_database('root')
 col = db.get_collection('user')
 
-@app.route("/login")
+@app.route("/login", methods=['GET',"POST"])
 def login():
+    if request.method == 'POST':
+        return 
     return render_template('login.html')
 
 @app.route('/join', methods=['GET',"POST"])
 def join():
-    if request.method == "POST" and request.form.get('submit_btn') == "join_form":
+    if request.method == "POST": #and request.form.get('submit_btn') == "join_form":
         email = request.form.get('join_email')
         pw = request.form.get('join_pw')
         pw2 = request.form.get('join_pw2')
         user_id = request.form.get('user_id')
-
+        print(email, pw, pw2, user_id)
         return render_template('join_success.html')
     else:
         return render_template('join.html')
