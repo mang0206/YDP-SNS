cd118469436154719db5fd9e01bd73383e821818
unknown <alswosp0206@naver.com>
2022-01-08 18:32:24 +0900

    [update] append friend

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import pymongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sns"
+app.config['JSON_AS_ASCII'] = False
 
 conn = pymongo.MongoClient("mongodb://root:study111@13.125.71.134:27017/root?authSource=admin")
 from app import views
