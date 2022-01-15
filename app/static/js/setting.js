30d20f4e2a1a7bb9485941400cc8491105e961da
현경 김 <ghwls9117@naver.com>
2022-01-15 18:10:13 +0900

    [Update] setting.js image upload function

---
+++ app/static/js/setting.js
@@ -0,0 +1,61 @@
+// 프로필 이미지 변경
+$(function() {
+    $("#profile_image_input").on('change', function(){
+    readURL(this);
+    });
+    console.log(this)
+
+});
+// 배경 이미지 변경
+$(function() {
+    $("#background_image_input").on('change', function(){
+        readURL(this);
+    });
+});
+
+var profile_image = document.getElementById('profile_image_input');
+var background_image = document.getElementById('background_image_input');
+
+function readURL(input) {
+    console.log(typeof(input.id))
+
+    // console.log(document.getElementById('background_image_input').getAttribute('id'))
+    
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+        
+        $('.background_image_input').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+
+    }
+
+}
+
+
+// var profile_image = [];
+
+// function change_image(){
+//     image = document.getElementById('profile_image_input').files;
+//     profile_image.push({
+//         "name" : image.name,
+//         "url" : URL.createObjectURL(image[0]),
+//         "file" : image[0]
+//     })
+//     console.log(profile_image)
+//     document.getElementById('profile_image').innerHTML = image_preview();
+// }
+// console.log(profile_image)
+
+// function image_preview(){
+//     console.log("image")
+//     var image = "";
+//     profile_image((i) => {
+//      image += `<div class="file_preview" id="file_preview">
+//      <img src="`+ i.url +`" alt="Image">
+//      </div>`;
+//     })
+//     return image;
+// }
--- app/static/js/user.js
+++ app/static/js/user.js
@@ -4,17 +4,17 @@
 
 // user 프로필 사진 및 배경 이미지
 // 업로드 할 이미지 미리보기(단일이미지)
-$(function() {
-    $("#popup_input_file").on('change', function(){
-    readURL(this);
-    });
-});
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-        $('#img_preview').attr('src', e.target.result);
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+// $(function() {
+//     $("#profile_image_input").on('change', function(){
+//     readURL(this);
+//     });
+// });
+// function readURL(input) {
+//     if (input.files && input.files[0]) {
+//         var reader = new FileReader();
+//         reader.onload = function (e) {
+//         $('.profile_image').attr('src', e.target.result);
+//         }
+//         reader.readAsDataURL(input.files[0]);
+//     }
+// }
