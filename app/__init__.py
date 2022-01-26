fe7c7c6a357b88eec3472ccc752131976f2fa29b
현경 김 <ghwls9117@naver.com>
2022-01-26 12:43:06 +0900

    [Update] views.py send_email 위치변경

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 import pymongo
-# 메일 인증
-from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -10,12 +8,9 @@ app.config['JSON_AS_ASCII'] = False
 # 메일 인증 기능
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 587
-app.config["MAIL_USERNAME"] = "herejddl@gmail.com" 
-app.config["MAIL_PASSWORD"] = "hioybzaxqcjpjshq"
+app.config["MAIL_USERNAME"] = "ydpsns.project@gmail.com" 
+app.config["MAIL_PASSWORD"] = "gqizwnzhwaxwrjjj"
 app.config["MAIL_USE_TLS"] = True 
-# app.config["MAIL_USE_SSL"] = True
-
-email = Mail(app)
 
 conn = pymongo.MongoClient("mongodb://root:study111@13.125.71.134:27017/root?authSource=admin")
 from app import views
