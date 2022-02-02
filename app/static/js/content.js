6bfb0ba9245f5b7ee6fce14589c98fc94c96f623
현경 김 <ghwls9117@naver.com>
2022-02-02 20:17:38 +0900

    [Update] content list frame

---
+++ app/static/js/content.js
@@ -0,0 +1,9 @@
+// 동일한 content 구조가 들어가는 page에 export
+
+// session user 더보기 btn
+$('.more_icon').click(function(){
+    
+    $('.more_icon_popup').toggleClass('none');
+    $('.more_icon_triangle').toggleClass('none');
+
+});
