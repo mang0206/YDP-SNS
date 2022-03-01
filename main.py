64cb9dd16ee57fa9155a8d40306b22cbb79e1b19
unknown <alswosp0206@naver.com>
2022-03-01 17:24:31 +0900

    [Fix] host 0.0.0.0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app, socketio
 
 if __name__ == "__main__" :
-    socketio.run(app, port=5000)
+    socketio.run(app, host='0.0.0.0',port=5000)
     # app.run(host='0.0.0.0',debug=True)
